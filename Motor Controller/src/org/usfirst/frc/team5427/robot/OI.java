6eb71fccd85a3a7cb7bbb6ebae8e673f429a7d68
Programming5427 <programming5427@gmail.com>
2018-09-29 14:14:15 -0700

    Working tilt. Need to fix Null Pointer Exception

--- Motor Controller/src/org/usfirst/frc/team5427/robot/OI.java
+++ Motor Controller/src/org/usfirst/frc/team5427/robot/OI.java
@@ -8,6 +8,8 @@ package org.usfirst.frc.team5427.robot;
 
 import org.usfirst.frc.team5427.robot.commands.IntakeActivateIn;
 import org.usfirst.frc.team5427.robot.commands.IntakeActivateOut;
+import org.usfirst.frc.team5427.robot.commands.TiltDown;
+import org.usfirst.frc.team5427.robot.commands.TiltUp;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
@@ -23,16 +25,21 @@ public class OI {
 	Button motorIntakeOut;
 	Button elevatorUp;
 	Button elevatorDown;
+	Button tiltUp, tiltDown;
 	public OI() {
 		joy1 = new Joystick(0);
 		motorIntakeIn = new JoystickButton(joy1, 7);
 		motorIntakeOut = new JoystickButton(joy1, 1);
 		elevatorUp = new JoystickButton(joy1, 5);
 		elevatorDown = new JoystickButton(joy1, 3);
+		tiltUp = new JoystickButton(joy1, 6);
+		tiltDown = new JoystickButton(joy1, 4);
 		motorIntakeIn.whenPressed(new IntakeActivateIn());
 		motorIntakeOut.whenPressed(new IntakeActivateOut());
 		elevatorUp.whenPressed(Robot.mou);
 		elevatorDown.whenPressed(Robot.mod);
+		tiltUp.whenPressed(new TiltUp());
+		tiltUp.whenPressed(new TiltDown());
 	}
 
 	/**
